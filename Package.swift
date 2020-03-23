746871e60f49a686f19b1b40e143dd009aba80cb
Finn Voorhees <finnvoorhees@gmail.com>
2020-03-23 13:52:46 +0000

    Add Package.swift

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "cmark-gfm",
+    products: [
+        .library(name: "cmark-gfm", targets: ["cmark-gfm"]),
+    ],
+    targets: [
+        .target(name: "cmark-gfm", path: "src", exclude: ["main.c"])
+    ]
+)

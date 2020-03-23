57294d185111cda6384bd4d025676efe0c611ad2
Finn Voorhees <finnvoorhees@gmail.com>
2020-03-23 14:07:38 +0000

    Add necessary headers

---
+++ src/cmark-gfm_version.h
@@ -0,0 +1,14 @@
+//
+//  cmark-gfm_version.h
+//  
+//
+//  Created by Finn Voorhees on 23/03/2020.
+//
+
+#ifndef CMARK_GFM_VERSION_H
+#define CMARK_GFM_VERSION_H
+
+#define CMARK_GFM_VERSION ((0 << 16) | (28 << 8) | 3)
+#define CMARK_GFM_VERSION_STRING "0.28.3"
+
+#endif
